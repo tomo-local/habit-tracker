911045718f18ea1626eaf0e229b3446b0f5428f9
tomo-local <tomohiro.togashi@gmail.com>
2026-07-13 14:43:52 +0900

    Config構造体を追加し、Googleカレンダートークンと習慣トラッカー設定を取得する機能を実装

--- main.go
+++ main.go
@@ -4,4 +4,33 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	config := Config{
+		googleCalendarTokenPath: ".",
+		habitTrackerConfigPath:  ".",
+	}
+
+	config.GetGoogleCalendarToken()
+	config.GetHabitTrackerConfig()
+}
+
+type Config struct {
+	googleCalendarTokenPath string
+	habitTrackerConfigPath  string
+}
+
+type HabitTrackerConfig struct {
+	Calendars    []string `json:"calendars"`
+	GroupByTitle bool     `json:"group_by_title"`
+	Habits       []string `json:"habits"`
+}
+
+func (c *Config) GetGoogleCalendarToken() ([]byte, error) {
+	fmt.Println("GetGoogleCalendarToken")
+	return nil, nil
+}
+
+func (c *Config) GetHabitTrackerConfig() (*HabitTrackerConfig, error) {
+	fmt.Println("GetHabitTrackerConfig")
+	return nil, nil
 }
